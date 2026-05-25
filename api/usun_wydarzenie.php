735608e934cc3cce4e8ea617db3eec8250159237
Przemek <100935707+PrzemekSwiecinski@users.noreply.github.com>
2026-05-25 18:30:30 +0200

    Update usun_wydarzenie.php

--- api/usun_wydarzenie.php
+++ api/usun_wydarzenie.php
@@ -1,6 +1,6 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: DELETE, OPTIONS");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 header("Content-Type: application/json; charset=utf-8");
 
@@ -9,40 +9,30 @@ if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     exit();
 }
 
-// Ustawienia połączenia z bazą danych
 $host = 'localhost';
 $dbname = 'culturify';
 $username = 'root';
 $password = '';
 
 try {
-    // Tworzenie połączenia z bazą danych
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    // Odbieranie danych z żądania POST
     $data = json_decode(file_get_contents("php://input"));
 
     if (isset($data->eventId)) {
         $eventId = $data->eventId;
 
-        // Zapytanie SQL do usunięcia wydarzenia
         $sql = "DELETE FROM wydarzenia WHERE id_wydarzenia = :eventId";
         $stmt = $pdo->prepare($sql);
         $stmt->bindParam(':eventId', $eventId, PDO::PARAM_INT);
-
-        // Wykonanie zapytania
         $stmt->execute();
 
-        // Zwrócenie odpowiedzi do klienta (React)
         echo json_encode(array("success" => true));
     } else {
-        // Jeśli brak eventId w żądaniu
-        echo json_encode(array("success" => false, "message" => "Nieprawidłowe żądanie"));
+        echo json_encode(array("success" => false, "message" => "Brak identyfikatora wydarzenia (eventId)"));
     }
 } catch (PDOException $e) {
-    // Obsługa błędów połączenia z bazą danych
-    echo json_encode(array("success" => false, "message" => "Błąd bazy danych: " . $e->getMessage()));
+    echo json_encode(array("success" => false, "message" => "Blad bazy danych: " . $e->getMessage()));
 }
 ?>
-
